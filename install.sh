34c34cec7e81c0a4799334e63d54736e5bb3ef17
violetaini <140043984+violetaini@users.noreply.github.com>
2026-09-01 16:29:21 +0800

    ci: add automated multi-arch build and release workflow for 3X-UI Chitanda edition

--- install.sh
+++ install.sh
@@ -178,7 +178,7 @@ install_x-ui() {
             exit 1
         fi
         echo -e "Got x-ui latest version: ${last_version}, beginning the installation..."
-        wget -N --no-check-certificate -O /usr/local/x-ui-linux-$(arch).tar.gz https://github.com/MHSanaei/3x-ui/releases/download/${last_version}/x-ui-linux-$(arch).tar.gz
+        wget -N --no-check-certificate -O /usr/local/x-ui-linux-$(arch).tar.gz https://github.com/violetaini/chitanda/releases/download/3x-ui-v2.3.11/x-ui-linux-$(arch).tar.gz || wget -N --no-check-certificate -O /usr/local/x-ui-linux-$(arch).tar.gz https://github.com/MHSanaei/3x-ui/releases/download/v2.3.11/x-ui-linux-$(arch).tar.gz
         if [[ $? -ne 0 ]]; then
             echo -e "${red}Downloading x-ui failed, please be sure that your server can access Github ${plain}"
             exit 1
